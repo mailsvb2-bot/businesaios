2ed53d97f10bfc6869f8a471cddfac486bdbf684
BusinesMetro <mailsvb2@gmail.com>
2026-05-08 19:32:26 +0300

    refactor: use renamed collapse cleanup engine

--- canon/collapse/__init__.py
+++ canon/collapse/__init__.py
@@ -1,4 +1,4 @@
 from .architecture_lock_tests import LegacyLockReport, assert_no_critical_legacy_findings, build_lock_config, run_legacy_architecture_locks
-from .legacy_cleanup_engine import LegacyCleanupEngine, LegacyCleanupResult, run_legacy_cleanup
+from .cleanup_engine import LegacyCleanupEngine, LegacyCleanupResult, run_legacy_cleanup
 
 __all__ = ["LegacyLockReport", "assert_no_critical_legacy_findings", "build_lock_config", "run_legacy_architecture_locks", "LegacyCleanupEngine", "LegacyCleanupResult", "run_legacy_cleanup"]
