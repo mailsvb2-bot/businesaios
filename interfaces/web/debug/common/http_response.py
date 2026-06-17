229d690d5547aacd4d5149e203cdd63eb3b69c5c
BusinesMetro <mailsvb2@gmail.com>
2026-06-17 23:04:04 +0300

    Accept content type on debug HTTP response

--- interfaces/web/debug/common/http_response.py
+++ interfaces/web/debug/common/http_response.py
@@ -7,6 +7,11 @@ class HttpResponse:
     status_code: int
     body: object
     headers: Mapping[str, str] | None = None
+    content_type: str = ""
+
+    def __post_init__(self) -> None:
+        if self.content_type and self.headers is None:
+            object.__setattr__(self, "headers", {"content-type": self.content_type})
 
 
 __all__ = ["HttpResponse"]
