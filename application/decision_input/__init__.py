a1a8dd382b2fc8634117935b0d6647e01dbe75cc
BusinesMetro <mailsvb2@gmail.com>
2026-07-06 00:40:18 +0300

    Fix decision input package import order

--- application/decision_input/__init__.py
+++ application/decision_input/__init__.py
@@ -1,6 +1,6 @@
-from __future__ import annotations
-
 """Application-level decision-input owners."""
+
+from __future__ import annotations
 
 from application.decision_input.decision_input_builder import build_decision_input_contract
 from application.decision_input.input_registry import InputRegistry
