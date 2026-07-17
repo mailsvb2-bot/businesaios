17431dd178ab796019837ff9529870104f3bea91
BusinesMetro <mailsvb2@gmail.com>
2026-07-17 20:14:56 +0300

    Remove executable recommendation builders from public application API

--- application/decision/__init__.py
+++ application/decision/__init__.py
@@ -1,29 +1,38 @@
 from __future__ import annotations
 
-from application.decision.action_dispatcher import ActionDispatcher
+from application.decision.action_dispatcher import (
+    ActionDispatcher,
+    DecisionEnvelopeRequiredError,
+)
 from application.decision.action_errors import (
     CANON_CORE_DECISION_ACTION_ERRORS,
     ActionExecutionRejectedError,
     DecisionApplicationError,
     InvalidActionError,
 )
-from application.decision.action_result import CANON_CORE_DECISION_ACTION_RESULT, ActionExecutionResult
+from application.decision.action_result import (
+    CANON_CORE_DECISION_ACTION_RESULT,
+    ActionExecutionResult,
+)
 from application.decision.action_result_presenter import (
     CANON_CORE_DECISION_ACTION_RESULT_PRESENTER,
     present_action_execution_result,
 )
 from application.decision.action_validator import ActionValidator
 from application.decision.decision_contract import (
+    CANON_RECOMMENDATION_CONTRACT_NO_EXECUTABLE_ACTION,
     NON_SOVEREIGN_ENGINE_ROLE,
     NON_SOVEREIGN_ENGINE_SURFACE,
-    build_executable_action,
-    build_executable_action_payload,
     canonical_request,
     start_trace,
 )
-from application.decision.decision_service import DecisionApplicationService, DecisionService
+from application.decision.decision_service import (
+    DecisionApplicationService,
+    DecisionService,
+)
 from application.decision.ports import (
     CANON_CORE_DECISION_APPLICATION_PORTS,
+    CANON_DECISION_EXECUTION_PORT_ENVELOPE_ONLY,
     DecisionExecutionPortProtocol,
     ObservabilityPortProtocol,
 )
@@ -40,16 +49,17 @@ __all__ = [
     "CANON_CORE_DECISION_ACTION_RESULT",
     "CANON_CORE_DECISION_ACTION_RESULT_PRESENTER",
     "CANON_CORE_DECISION_APPLICATION_PORTS",
+    "CANON_DECISION_EXECUTION_PORT_ENVELOPE_ONLY",
+    "CANON_RECOMMENDATION_CONTRACT_NO_EXECUTABLE_ACTION",
     "DecisionApplicationError",
     "DecisionApplicationService",
+    "DecisionEnvelopeRequiredError",
     "DecisionExecutionPortProtocol",
     "DecisionService",
     "InvalidActionError",
     "NON_SOVEREIGN_ENGINE_ROLE",
     "NON_SOVEREIGN_ENGINE_SURFACE",
     "ObservabilityPortProtocol",
-    "build_executable_action",
-    "build_executable_action_payload",
     "canonical_request",
     "present_action_execution_result",
     "start_trace",
