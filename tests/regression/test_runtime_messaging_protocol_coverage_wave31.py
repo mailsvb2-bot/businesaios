70d922c6d926a91cf0ab7f69486e1999d1b437d7
BusinesMetro <mailsvb2@gmail.com>
2026-07-22 01:00:27 +0300

    Wave 31: remove duplicate protocol regression location

--- tests/regression/test_runtime_messaging_protocol_coverage_wave31.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-
-from runtime.messaging.adapter_protocol import MessageAdapter
-
-
-def test_message_adapter_protocol_runtime_surface_is_non_executing() -> None:
-    assert MessageAdapter.send(object(), object()) is None
