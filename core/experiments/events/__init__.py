e16570f48f38b6b60b8eab29cf83e3712cee9aee
BusinesMetro <mailsvb2@gmail.com>
2026-08-02 18:30:03 +0300

    Expose live canary events through existing package

--- core/experiments/events/__init__.py
+++ core/experiments/events/__init__.py
@@ -2,10 +2,32 @@ from core.experiments.events.assignment_recorded import AssignmentRecorded
 from core.experiments.events.experiment_evaluated import ExperimentEvaluated
 from core.experiments.events.experiment_registered import ExperimentRegistered
 from core.experiments.events.rollout_blocked import RolloutBlocked
+from core.experiments.live_canary_events import (
+    BUSINESS_OUTCOME_OBSERVED,
+    CANARY_AUTO_ROLLED_BACK,
+    CANARY_GUARDRAIL_BREACHED,
+    CANARY_PROMOTED,
+    CANDIDATE_ACTION_EXECUTED,
+    CONTROL_ACTION_EXECUTED,
+    EXPERIMENT_ASSIGNMENT,
+    EXPERIMENT_CREATED,
+    LIVE_CANARY_EVENT_TYPES,
+    is_live_canary_event,
+)
 
 __all__ = [
     "AssignmentRecorded",
+    "BUSINESS_OUTCOME_OBSERVED",
+    "CANARY_AUTO_ROLLED_BACK",
+    "CANARY_GUARDRAIL_BREACHED",
+    "CANARY_PROMOTED",
+    "CANDIDATE_ACTION_EXECUTED",
+    "CONTROL_ACTION_EXECUTED",
+    "EXPERIMENT_ASSIGNMENT",
+    "EXPERIMENT_CREATED",
     "ExperimentEvaluated",
     "ExperimentRegistered",
+    "LIVE_CANARY_EVENT_TYPES",
     "RolloutBlocked",
+    "is_live_canary_event",
 ]
