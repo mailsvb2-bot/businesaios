53a37f7ce87d6007bee889d4243961070ef43449
BusinesMetro <mailsvb2@gmail.com>
2026-08-08 17:09:03 +0300

    Lock certified WhatsApp inbound capability truth

--- tests/unit/application/test_provider_truth_contract_only_fail_closed.py
+++ tests/unit/application/test_provider_truth_contract_only_fail_closed.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from application.business_autonomy.integration_capability_catalog import CapabilityStatus, capability_map
 from application.business_autonomy.provider_truth_matrix import ProviderTruthStatus, provider_truth_map
 from application.public_site.cta_intake import public_integration_marketplace
 
@@ -14,3 +15,12 @@ def test_contract_only_provider_never_becomes_customer_selectable_from_runtime_p
     for key in contract_only & marketplace.keys():
         assert marketplace[key]['selectable'] is False
         assert marketplace[key]['read_supported'] is False
+
+
+def test_certified_whatsapp_inbound_is_partial_read_verify_without_write() -> None:
+    capability = capability_map()['interaction.whatsapp']
+    assert capability.status is CapabilityStatus.PARTIAL
+    assert capability.read_supported is True
+    assert capability.verify_supported is True
+    assert capability.requires_webhook is True
+    assert capability.write_supported is False
