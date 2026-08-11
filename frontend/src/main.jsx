ba0aeeb331da5c1672baf108c98e0777a041373f
BusinesMetro <mailsvb2@gmail.com>
2026-08-11 21:08:42 +0300

    Expose acquisition calculator from the product shell

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { AcquisitionCalculatorPage, CalculatorLauncher } from "./AcquisitionCalculatorPage";
 import { App } from "./App";
 import "./styles.css";
 
+const calculatorRoute = window.location.pathname.replace(/\/+$/, "") === "/calculator";
+const apiBase = import.meta.env.VITE_API_BASE || "https://api.businessaios.ru";
+
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {calculatorRoute ? <AcquisitionCalculatorPage apiBase={apiBase} /> : <><App /><CalculatorLauncher /></>}
   </React.StrictMode>
 );
