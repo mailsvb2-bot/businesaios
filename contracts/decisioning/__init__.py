d050b6f8a72ea315c36ae270f86e1e3f798d3179
BusinesMetro <mailsvb2@gmail.com>
2026-08-29 09:23:57 +0300

    Canon: avoid duplicate package-level decision ownership

--- contracts/decisioning/__init__.py
+++ contracts/decisioning/__init__.py
@@ -4,12 +4,9 @@ from contracts.decisioning.decision_envelope_contract import DecisionEnvelopeCon
 from contracts.decisioning.decision_input_contract import DecisionInputContract
 from contracts.decisioning.recommendation_packet_contract import RecommendationPacketContract
 from contracts.decisioning.reward_signal_contract import RewardSignalContract
-from contracts.decisioning.sovereign_decision_contract import Decision, DecisionEnvelope
 from contracts.decisioning.world_state_contract import WorldStateContract
 
 __all__ = [
-    "Decision",
-    "DecisionEnvelope",
     "DecisionEnvelopeContract",
     "DecisionInputContract",
     "RecommendationPacketContract",
