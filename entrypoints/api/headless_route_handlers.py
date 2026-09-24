ca09f767376dcb495df42c8dc866283cd7cbb08e
BusinesMetro <mailsvb2@gmail.com>
2026-09-24 17:34:43 +0300

    compat: accept pre-goal_id headless request shapes

--- entrypoints/api/headless_route_handlers.py
+++ entrypoints/api/headless_route_handlers.py
@@ -45,7 +45,7 @@ class HeadlessRouteHandlers:
             GoalExecutionRequest(
                 goal=request.goal,
                 business_id=request.business_id,
-                goal_id=request.goal_id,
+                goal_id=getattr(request, "goal_id", None),
                 tenant_id=request.tenant_id,
                 user_id=request.user_id,
                 region=request.region,
